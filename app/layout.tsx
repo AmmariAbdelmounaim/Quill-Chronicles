4f2356670a6a76d377e06fd0aee55bee1717b7b9
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-14 18:51:38 +0100

    Integrating shadcn/ui as the project ui library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -10,13 +11,24 @@ export const metadata = {
   title: "Next.js Full Stack blog app",
 };
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html
+      lang="en"
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
       <body>
         <main>{children}</main>
       </body>
