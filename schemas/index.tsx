f5a10044858ab6877a8246a59d45750ace9d4644
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 08:48:24 +0100

    refactor: Remove unused code and update page layout

--- schemas/index.tsx
+++ schemas/index.tsx
@@ -1,3 +1,4 @@
+import { JSONContent } from "novel";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
@@ -18,3 +19,19 @@ export const RegisterSchema = z.object({
     message: "Minimum 6 characters required",
   }),
 });
+
+// const MarksSchema = z.object({
+//   type: z.string(),
+//   attrs: z.record(z.any()).optional(),
+// });
+
+// // Define the recursive schema for JSONContent
+// export const JSONContentSchema: z.ZodType<JSONContent> = z.lazy(() =>
+//   z.object({
+//     type: z.string().optional(),
+//     attrs: z.record(z.any()).optional(),
+//     content: z.array(JSONContentSchema).optional(), // Recursive definition
+//     marks: z.array(MarksSchema).optional(),
+//     text: z.string().optional(),
+//   })
+// );
