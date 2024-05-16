ebfd1c7ddd9fb5b0ad11cbe5ae907a97aab1e2d0
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 08:45:21 +0100

    refactor: Update NewArticle page to use container layout

--- app/new-article/page.tsx
+++ app/new-article/page.tsx
@@ -1,5 +1,9 @@
 import NovelEditor from "@/components/novelEditor";
 
 export default function NewArticle() {
-  return <NovelEditor />;
+  return (
+    <main className="container flex justify-center px-4 py-12 sm:px-6 lg:px-8">
+      <NovelEditor />
+    </main>
+  );
 }
