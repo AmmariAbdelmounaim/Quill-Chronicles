b9a3b2c4287646b245a9b81407873906b40e4a9e
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 08:45:14 +0100

    refactor: Add page component for editing articles

---
+++ app/edit-article/[articleId]/page.tsx
@@ -0,0 +1,29 @@
+import NovelEditor from "@/components/novelEditor";
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { JSONContent } from "novel";
+
+export default async function EditArticle({
+  params,
+}: {
+  params: { articleId: string };
+}) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("articles")
+    .select("*")
+    .eq("id", params.articleId)
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  const initialContent: JSONContent = data.content as unknown as JSONContent;
+  console.log(initialContent);
+  return (
+    <main className="container flex justify-center px-4 py-12 sm:px-6 lg:px-8">
+      <NovelEditor initialContent={initialContent} />
+    </main>
+  );
+}
