bf9888796e185798a0d4357a3e663e51d845ce12
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 11:23:40 +0100

    feat: Add useEffect hook to novelEditor component

--- components/novelEditor/index.tsx
+++ components/novelEditor/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import {
   EditorRoot,
