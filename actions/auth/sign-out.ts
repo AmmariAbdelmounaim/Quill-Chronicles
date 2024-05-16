c7933d45447483aff4d174821abbcb07701088fb
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 08:44:30 +0100

    refactor: Add lazy loading for images in sign-out.ts

--- actions/auth/sign-out.ts
+++ actions/auth/sign-out.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function signOut() {
