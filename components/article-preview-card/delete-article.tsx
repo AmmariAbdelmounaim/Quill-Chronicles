c5d77130c50f844e16090ae8765d720986f95caa
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 11:23:28 +0100

    refactor: Add delete functionality to article preview card

---
+++ components/article-preview-card/delete-article.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Trash2 } from "lucide-react";
+import { useTransition } from "react";
+import { useToast } from "../ui/use-toast";
+import { removeArticle } from "@/actions/remove-article";
+
+interface DeleteArticleProps {
+  articleId: string;
+}
+
+export default function DeleteArticle({ articleId }: DeleteArticleProps) {
+  const [isPending, startTransition] = useTransition();
+  const { toast } = useToast();
+
+  const handleDeleteArticle = (articleId: string) => {
+    startTransition(async () => {
+      const data = await removeArticle(articleId);
+      if (data.error) {
+        toast({
+          variant: "destructive",
+          title: data.error,
+        });
+      }
+      toast({
+        title: data.success,
+      });
+    });
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Trash2 className="size-5 stroke-red-600 opacity-80 hover:opacity-100" />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            article from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => handleDeleteArticle(articleId)}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
