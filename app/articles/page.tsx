c97d64e7a322fc66b3e34684908c89c9acb46cb7
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-16 11:23:12 +0100

    refactor: Fetch and display user-specific articles on ArticlesPage

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,8 +1,29 @@
+import { fetchUserProfile } from "@/actions/auth/fetch-user-profile";
 import ArticlePreviewCard from "@/components/article-preview-card";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { Tables } from "@/types/supabase";
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
 import Link from "next/link";
 
-export default function ArticlesPage() {
+export default async function ArticlesPage() {
+  const supabase = createClient();
+  const userProfile = await fetchUserProfile();
+  let articles: Tables<"articles">[] = [];
+
+  if (userProfile) {
+    const { data, error } = await supabase
+      .from("articles")
+      .select("*")
+      .eq("profile_id", userProfile?.id);
+    if (error) {
+      throw new Error(error.message);
+    }
+    articles = data;
+  }
+
+  revalidatePath("/articles");
+
   return (
     <main className="container mx-auto px-4 py-12 sm:px-6 lg:px-8 ">
       <div className="flex justify-between">
@@ -18,12 +39,9 @@ export default function ArticlesPage() {
       </div>
 
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        <ArticlePreviewCard />
-        <ArticlePreviewCard />
-        <ArticlePreviewCard />
-        <ArticlePreviewCard />
-        <ArticlePreviewCard />
-        <ArticlePreviewCard />
+        {articles.map((article) => (
+          <ArticlePreviewCard key={article.id} article={article} />
+        ))}
       </div>
     </main>
   );
