359d9b7f7a78fa8a90f134e33b6a7fcffe9dfe4d
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-17 06:43:04 +0100

    chore: Disable React strict mode in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   // (Optional) Export as a standalone site
   // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
   output: "standalone", // Feel free to modify/remove this option
-
+  reactStrictMode: false,
   // Indicate that these packages should not be bundled by webpack
   experimental: {
     serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
